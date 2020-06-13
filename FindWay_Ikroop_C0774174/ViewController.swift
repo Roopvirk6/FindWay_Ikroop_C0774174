d02d2e75e636084b3832bcdb4f6e13d52b448c02
Roopvirk6 <virkikroop13@gmail.com>
2020-06-13 17:25:32 -0400

    [ikroop] changes

--- FindWay_Ikroop_C0774174/ViewController.swift
+++ FindWay_Ikroop_C0774174/ViewController.swift
@@ -15,8 +15,13 @@ class ViewController: UIViewController{
     @IBOutlet weak var mapView: MKMapView!
     
     
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    
+    
    let locationManager = CLLocationManager()
     let regionInMeters : Double = 100000
+    var previousLocations : CLLocation?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,10 +57,7 @@ class ViewController: UIViewController{
     {
         switch CLLocationManager.authorizationStatus(){
         case .authorizedWhenInUse:
-            mapView.showsUserLocation = true
-            centerViewOnUserLocation()
-            locationManager.startUpdatingLocation()
-            break
+            startTrackingUserLocation()
         case .denied:
             //show alert
             break
@@ -70,23 +72,75 @@ class ViewController: UIViewController{
         }
     }
     
+    func startTrackingUserLocation() {
+        mapView.showsUserLocation = true
+        centerViewOnUserLocation()
+        locationManager.startUpdatingLocation()
+        previousLocations = getCenterLocation(for: mapView)
+        
+    }
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation
+    {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
 
 
 }
 extension ViewController:CLLocationManagerDelegate
 {
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location = locations.last else{ return}
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
-        mapView.setRegion(region, animated: true)
+    //func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+     //   guard let location = locations.last else{ return}
+     //   let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+     //   let region = MKCoordinateRegion.init(center: center, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+     //   mapView.setRegion(region, animated: true)
         
-    }
+   // }
+    
+    
+    
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         checkLocationAuthorization()
         
     }
     
+    
+}
+
+extension ViewController:MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geocoder = CLGeocoder()
+        guard let previousLocations = self.previousLocations else {
+            return
+        }
+        guard center.distance(from: previousLocations) > 50 else {return}
+        self.previousLocations = center
+        
+        geocoder.reverseGeocodeLocation(center){[weak self] (placemarks, error) in
+            guard let self = self else {return}
+            
+            if let _ = error{
+                //todo list
+                return
+                
+            }
+            guard let placemark = placemarks?.first else {
+                //todo list
+                return
+            }
+            
+            let streetNumber = placemark.subThoroughfare ?? ""
+            let streetName = placemark.thoroughfare ?? ""
+            
+            DispatchQueue.main.async {
+                self.addressLabel.text = "\(streetNumber) \(streetName)"
+            }
+            
+            
+        }
+    }
     
 }
 
